959f7d2566981ff35761187525d241ae596fc061
Aniruddha <anirudhsurse143@gmail.com>
2024-04-28 03:21:06 +0530

    update start_cotainer.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-docker pull ani007s/python-server:latest
+sudo docker pull ani007s/python-server:latest
 
-docker run -d -p 5000:5000 ani007s/python-server:latest
+sudo docker run -d -p 5000:5000 ani007s/python-server:latest
